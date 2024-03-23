0ec3507719875510286a51222c6d719a341fd6d4
Harley Kradovill <harleykradovill@gmail.com>
2024-03-23 00:28:55 -0600

    ya it works

--- vehreg_client.lua
+++ vehreg_client.lua
@@ -1,15 +1,35 @@
 json = require("json")
 
--- COMMANDS:
+RegisterCommand('vehreg', function(source, args, rawCommand)
 
-RegisterCommand('vehreg', function()
-    TriggerServerEvent('fetchIncidentDetails')
+   if #args < 1 then
+        print("Usage: /vehreg <SSN>")
+        return
+    end
+    local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+    local plate = GetVehicleNumberPlateText(vehicle)
+    local ssn = args[1]
+    local make = GetLabelText(GetMakeNameFromVehicleModel(GetEntityModel(vehicle)))
+    local model = GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)))
+    local color = GetVehiclePrimaryColorText(vehicle)
+
+    print("Sending vehicle registration request to server...")
+    TriggerServerEvent('sendVehicleRegistration', plate, ssn, make, model, color)
 end, false)
 
--- UNIVERSAL FUNCTION TO DRAW NOTIFICATION
-
-function drawNotification(message)
-    SetNotificationTextEntry("STRING")
-    AddTextComponentString(message)
-    DrawNotification(false, true)
+function GetVehiclePrimaryColorText(vehicle)
+    local primaryColorID, _ = GetVehicleColours(vehicle)
+    local colorJson = LoadResourceFile(GetCurrentResourceName(), "vehicle-colors.json")
+    if colorJson then
+        local colors = json.decode(colorJson)
+        for _, color in ipairs(colors) do
+            if color.ID == tostring(primaryColorID) then
+                return color.Description
+            end
+        end
+        return "Unknown Color"
+    else
+        print("Error: vehicle-colors.json not found or unable to read.")
+        return "Unknown Color"
+    end
 end
--- vehreg_server.lua
+++ vehreg_server.lua
@@ -1,3 +1,39 @@
-local cadURL = 'https://coastalstateroleplay.bubbleapps.io/api/1.1/wf/registervehicle?api_token=[REDCATED]'
+local cadURL = '[redacted]'
 
--- Sending current vehicle info to CAD
+RegisterServerEvent('sendVehicleRegistration')
+AddEventHandler('sendVehicleRegistration', function(plate, ssn, make, model, color)
+    local playerSource = source
+    local playerIdentifiers = GetPlayerIdentifiers(playerSource)
+    local discordId = nil
+
+    for _, identifier in pairs(playerIdentifiers) do
+        if string.match(identifier, "discord:") then
+            discordId = string.sub(identifier, 9)
+            break
+        end
+    end
+
+    if discordId then
+        local vehicleProps = {
+            plate = plate,
+            ssn = ssn,
+            discordid = discordId,
+	    make = make,
+	    model = model,
+	    color = color
+        }
+
+        print(json.encode(vehicleProps))
+
+        print("Sending vehicle registration request to website API...")
+        PerformHttpRequest(cadURL, function(err, text, headers)
+            if err == 200 then
+                print("Vehicle registration request sent successfully.")
+            else
+                print("Error: HTTP request failed. Status code: " .. tostring(err))
+            end
+        end, 'POST', json.encode(vehicleProps), { ["Content-Type"] = 'application/json' })
+    else
+        print("Error: Discord ID not found for the player.")
+    end
+end)
