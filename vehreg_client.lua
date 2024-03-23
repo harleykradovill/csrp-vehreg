f7529a4a8cef5dbd7cdbe1fd709ce41595794fca
Harley Kradovill <harleykradovill@gmail.com>
2024-03-23 11:32:35 -0600

    error checking and draw notification

--- vehreg_client.lua
+++ vehreg_client.lua
@@ -1,20 +1,14 @@
 json = require("json")
 
 RegisterCommand('vehreg', function(source, args, rawCommand)
-
-   if #args < 1 then
-        print("Usage: /vehreg <SSN>")
-        return
-    end
     local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
     local plate = GetVehicleNumberPlateText(vehicle)
-    local ssn = args[1]
     local make = GetLabelText(GetMakeNameFromVehicleModel(GetEntityModel(vehicle)))
     local model = GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)))
     local color = GetVehiclePrimaryColorText(vehicle)
 
-    print("Sending vehicle registration request to server...")
-    TriggerServerEvent('sendVehicleRegistration', plate, ssn, make, model, color)
+    -- print("Sending vehicle registration request to server...")
+    TriggerServerEvent('sendVehicleRegistration', plate, make, model, color)
 end, false)
 
 function GetVehiclePrimaryColorText(vehicle)
@@ -27,9 +21,23 @@ function GetVehiclePrimaryColorText(vehicle)
                 return color.Description
             end
         end
-        return "Unknown Color"
+        return "Unknown"
     else
         print("Error: vehicle-colors.json not found or unable to read.")
-        return "Unknown Color"
+        return "Unknown"
     end
+end
+
+RegisterNetEvent("showVehicleRegistrationNotification")
+AddEventHandler("showVehicleRegistrationNotification", function(message)
+    drawNotification(message)
+end)
+
+
+-- UNIVERSAL FUNCTION TO DRAW NOTIFICATION
+
+function drawNotification(message)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(message)
+    DrawNotification(false, true)
 end
--- vehreg_server.lua
+++ vehreg_server.lua
@@ -1,7 +1,7 @@
 local cadURL = '[redacted]'
 
 RegisterServerEvent('sendVehicleRegistration')
-AddEventHandler('sendVehicleRegistration', function(plate, ssn, make, model, color)
+AddEventHandler('sendVehicleRegistration', function(plate, make, model, color)
     local playerSource = source
     local playerIdentifiers = GetPlayerIdentifiers(playerSource)
     local discordId = nil
@@ -16,7 +16,6 @@ AddEventHandler('sendVehicleRegistration', function(plate, ssn, make, model, col
     if discordId then
         local vehicleProps = {
             plate = plate,
-            ssn = ssn,
             discordid = discordId,
 	    make = make,
 	    model = model,
@@ -25,15 +24,17 @@ AddEventHandler('sendVehicleRegistration', function(plate, ssn, make, model, col
 
         print(json.encode(vehicleProps))
 
-        print("Sending vehicle registration request to website API...")
+        -- print("Sending vehicle registration request to website API...")
         PerformHttpRequest(cadURL, function(err, text, headers)
             if err == 200 then
-                print("Vehicle registration request sent successfully.")
+                -- print("Vehicle registration request sent successfully.")
+		TriggerClientEvent('showVehicleRegistrationNotification', playerSource, text)
             else
-                print("Error: HTTP request failed. Status code: " .. tostring(err))
+                -- print("Error: HTTP request failed. Status code: " .. tostring(err))
+		TriggerClientEvent('showVehicleRegistrationNotification', playerSource, "Error: Vehicle registration failed due to API.")
             end
         end, 'POST', json.encode(vehicleProps), { ["Content-Type"] = 'application/json' })
     else
-        print("Error: Discord ID not found for the player.")
+        -- print("Error: Discord ID not found for the player.")
     end
 end)
